7ce232ca0f29f8822d97315a67613a9133bbd8b0
Marcilio Leite <marcilioleite@live.com>
2013-05-15 10:48:47 -0300

    -m

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1/saude'
+app.config['BOOTSTRAP_FONTAWESOME'] = True
 db = SQLAlchemy(app)
 
 from app import models, views
--- app/views.py
+++ app/views.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 from app import app
 from models import Medico, Agendamento
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/')
 def index():
     m = Medico.query.get(1)
